ea2d63c3c0941c1d531f0b821b6742e999cf4b66
exelimpichment <oldestspy@gmail.com>
2025-04-03 21:35:32 +0200

    Enhance AppController to include getPort method and integrate ConfigModule in EnvModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,10 @@
 import { AppController } from '@/app.controller';
 import { EnvModule } from '@/env/env.module';
-import { ConfigModule } from '@nestjs/config';
 import { AppService } from '@/app.service';
 import { Module } from '@nestjs/common';
-import { envSchema } from '@/env/env';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      validate: (env) => envSchema.parse(env),
-      isGlobal: true,
-    }),
-    EnvModule,
-  ],
+  imports: [EnvModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/env/env.module.ts
+++ src/env/env.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { EnvService } from '@/env';
+import { envSchema } from '@/env/env';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+    }),
+  ],
   providers: [EnvService],
   exports: [EnvService],
 })
